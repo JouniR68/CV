faf60e31373a0bf718b7d32d0218a92b19b6c49c
riimalaj <jriimala@gmail.com>
2024-01-07 14:17:19 +0200

    work and education basic stuff

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,11 +1,36 @@
 import JsonData from "../../data/datapkg.json";
-//import remover from "../utils/common"
+import remover from "../utils/common"
 
 export default function Education() {
+
+  let i = 0;
+  const issueData = JsonData.education.map((j) => {
+    return j;
+  });
 
   
 
   return (
-  <h1>Education</h1>
-    );
+    <table key={i++} className="taulu">
+      {issueData?.map((t) => (
+        <div key={i++} className="row">
+          <div key={i++} className="cell">
+            {t?.Item}
+          </div>
+          <div key={i++} className="cell">
+            {t?.When}
+          </div>
+          <div key={i++} className="cell">
+            {t?.Topics}
+          </div>
+          <div key={i++} className="cell">
+            {t?.Degree}
+          </div>
+          <div key={i++} className="cell">
+            <button onClick={() => remover("work", t.id)}>x</button>
+          </div>
+        </div>
+      ))}
+    </table>
+  );
 }
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,14 +1,31 @@
 import JsonData from "../../data/datapkg.json";
-//import remover from "../utils/common"
+import remover from "../utils/common"
 
-export default function Education() {
+export default function Work() {
 
-  /*
+  let i = 0;
+  const issueData = JsonData.workhistory.map((j) => {
+    return j;
+  });
+
+  
+
+  return (
     <table key={i++} className="taulu">
       {issueData?.map((t) => (
         <div key={i++} className="row">
           <div key={i++} className="cell">
-            {t?.name}: {t?.kuvaus}
+            {t?.Company}
+          </div>
+          <div key={i++} className="cell">
+            {t?.Duration}
+          </div>
+          <div key={i++} className="cell">
+            {t?.Roles.map(r => r
+            )}
+          </div>
+          <div key={i++} className="cell">
+            {t?.Locations.map(l => l)}
           </div>
           <div key={i++} className="cell">
             <button onClick={() => remover("work", t.id)}>x</button>
@@ -16,10 +33,5 @@ export default function Education() {
         </div>
       ))}
     </table>
-
-  */
-
-  return (
-    <h1>Work</h1>
   );
 }
