0d1d652829060e9828f7ba266205b97230e8681d
riimalaj <jriimala@gmail.com>
2024-01-07 11:11:03 +0200

    Profile

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,12 +1,24 @@
 import JsonData from "../../data/datapkg.json";
 
 export default function Education() {
-
-  
+  let i = 0;
+  const profileData = JsonData.Profile.map((j) => {
+    return j;
+  });
 
   return (
-    <div className = "profileCard"> 
-      <h>Profile</h>
+    <div className="profile--container">
+      {profileData.map((profile) => (
+        <div key={i++} className="profile--card">          
+          <img key={i++} src={profile.Photo} alt="Jouni Riimala" />
+          <table className = "taulu">
+          <td className="row">{profile.Name}</td>
+            <td className="row">{profile.BirthPlace}</td>
+            <td className="row">{profile.Profession}</td>
+            <td className="row">{profile.Familly}</td>
+          </table>
+        </div>
+      ))}
     </div>
-    );
+  );
 }
