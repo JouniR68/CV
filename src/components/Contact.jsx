e6c236040195689c95a8a445a59676f41290a6d8
riimalaj <jriimala@gmail.com>
2024-01-31 18:36:49 +0200

    net cv

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,13 +1,56 @@
+import Button from "@mui/material/Button";
+import ButtonGroup from "@mui/material/ButtonGroup";
+import Box from "@mui/material/Box";
+import IconButton from "@mui/material/IconButton";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+
 export default function Contact() {
+  function linkedIn() {
+    window.open("https://www.linkedin.com/in/jouni-riimala-04330", "_blank");
+  }
 
-    return(
-        <>
-        <h3 className="cv--contact-title">Contacting</h3>
-        <p></p>
-        <div className = "cv--contact-text">
-            Let's start with the email so send me an email and lets more from there, waiting your contact request on <a href = "mailto jriimala@gmail.com"></a>.
+  function fb() {
+    window.open("https://www.facebook.com/jriimala", "_blank");
+  }
+
+  function insta() {
+    window.open("https://www.instagram.com/jriimala/", "_blank");
+  }
+
+  const contactText =
+    "Excellent, let's start with the email so send me an email to jriimala@gmail.com and lets take it from there.";
+  const socialText = "Checkout my social media pages"
+
+  return (
+    <div className="contact-card">
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "10px",
+          alignItems: "center",
+          "& > *": {
+            m: 1,
+          },
+        }}
+      >        
+        <div className = "contact-info">
+            {contactText}
         </div>
-        </>
-    )
-
+        <ButtonGroup variant="outlined" aria-label="outlined button group" className = "socialButtons">
+          <IconButton>
+            <FacebookIcon onClick={fb}/>
+          </IconButton>
+          <IconButton>
+            <InstagramIcon onClick={insta}/>
+          </IconButton>
+          <IconButton>
+            <LinkedInIcon onClick={linkedIn} />
+          </IconButton>
+        </ButtonGroup>
+      </Box>
+    </div>
+  );
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,9 @@ export default function Home() {
       <h1>Welcome</h1>
       <div className="home-container">
 
-        Sole purpose of these pages is to act as a net based CV. Naturally as an capitalist open for new contracts (who wouldn't).  
+        Sole purpose of these pages is to act as a net based CV. Naturally as an capitalist open for new offers (who wouldn't).
+        <p></p>
+        Note that I haven't prepared pages for the mobile usage so if using mobile, sorry in advanced.  
 
       </div>
     </>
