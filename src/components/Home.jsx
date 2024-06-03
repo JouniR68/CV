e95a9edbc7a40dbaefdb7d4c8676fc21b0f86aca
riimalaj <jriimala@gmail.com>
2024-06-03 08:29:17 +0300

    CV tuning

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,9 +8,6 @@ export default function Home() {
       <div className='home-container'>
         Sole purpose of these pages is to act as a net based CV. Naturally as an
         capitalist open for new offers (who wouldn't).
-        <p></p>
-        Note that I haven't prepared pages for the mobile usage so if using
-        mobile, sorry in advanced.
       </div>
     </>
   );
