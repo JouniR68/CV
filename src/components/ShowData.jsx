1675082c8cf4391002a54488646d4023362ad062
JouniR68 <jriimala@gmail.com>
2024-07-07 11:04:59 +0300

    ShowData to show also found locations

--- src/components/ShowData.jsx
+++ src/components/ShowData.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react"
 import { collection, getDocs } from "firebase/firestore"
 import { db } from "../firebase"
-import {nanoid} from 'nanoid'
+import { nanoid } from 'nanoid'
 
 import {
 	Table,
@@ -14,9 +14,10 @@ import {
 } from '@mui/material';
 
 const ShowCustomers = () => {
-	//<TableCell>{t.firmId}</TableCell>
+	const [locations, setLocations] = useState([])
 	const [customer, setCustomer] = useState([])
 	const [error, setError] = useState("")
+
 
 	const getContracts = async () => {
 		try {
@@ -39,44 +40,89 @@ const ShowCustomers = () => {
 		}
 	}
 
+
+	const getLocations = async () => {
+		try {
+			const customerRef = collection(db, "Locations")
+			const querySnapshot = await getDocs(customerRef)
+			const data = querySnapshot.docs.map((doc) => ({
+				id: doc.id,
+				...doc.data(),
+			}))
+			console.log("Customer data: ", data)
+			setLocations(data)
+		} catch (error) {
+			console.error("Error fetching data: ", error)
+			setError("Error fetching data, pls contact site admin.")
+			throw {
+				message: "Datan haku epäonnistui",
+				statusText: "Failas",
+				status: 403,
+			}
+		}
+	}
+
+
 	useEffect(() => {
 		getContracts()
 	}, [])
 
 	return (
-		
+
 		<TableContainer component={Paper}>
 			{error.length > 1 && <h3>{error}</h3>}
 			<Table sx={{ minWidth: 650 }} aria-label="simple table" key={nanoid()}>
 
 				<TableHead>
 					<TableRow>
-						<TableCell sx={{fontWeigth:'bold'}} align="left">First Name</TableCell>
-						<TableCell sx={{fontWeigth:'bold'}} align="right">Last Name</TableCell>
-						<TableCell sx={{fontWeigth:'bold'}} align="left">Email</TableCell>
-						<TableCell sx={{fontWeigth:'bold'}} align="left">Phone</TableCell>
-						<TableCell sx={{fontWeigth:'bold'}} align="left">Firm Id</TableCell>
-						<TableCell sx={{fontWeigth:'bold'}} align="left">Description</TableCell>
-						<TableCell sx={{fontWeigth:'bold'}} align="left">Created</TableCell>
+						<TableCell sx={{ fontWeigth: 'bold' }} align="left">First Name</TableCell>
+						<TableCell sx={{ fontWeigth: 'bold' }} align="right">Last Name</TableCell>
+						<TableCell sx={{ fontWeigth: 'bold' }} align="left">Email</TableCell>
+						<TableCell sx={{ fontWeigth: 'bold' }} align="left">Phone</TableCell>
+						<TableCell sx={{ fontWeigth: 'bold' }} align="left">Firm Id</TableCell>
+						<TableCell sx={{ fontWeigth: 'bold' }} align="left">Description</TableCell>
+						<TableCell sx={{ fontWeigth: 'bold' }} align="left">Created</TableCell>
 					</TableRow>
 				</TableHead>
 
-				
+
 				{customer.map((t) => (
-						<TableBody key={nanoid()}>
-							<TableRow key={nanoid()}>
-								<TableCell>{t.fName}</TableCell>
-								<TableCell>{t.lName}</TableCell>
-								<TableCell>{t.address}</TableCell>
-								<TableCell>{t.email}</TableCell>
-								<TableCell>{t.phone}</TableCell>								
-								<TableCell>{t.description}</TableCell>
-								<TableCell>{t.pvm}</TableCell>
-							</TableRow>
-						</TableBody>
-					))}
+					<TableBody key={nanoid()}>
+						<TableRow key={nanoid()}>
+							<TableCell>{t.fName}</TableCell>
+							<TableCell>{t.lName}</TableCell>
+							<TableCell>{t.address}</TableCell>
+							<TableCell>{t.email}</TableCell>
+							<TableCell>{t.phone}</TableCell>
+							<TableCell>{t.description}</TableCell>
+							<TableCell>{t.pvm}</TableCell>
+						</TableRow>
+					</TableBody>
+				))}
 			</Table>
+
+			<Table sx={{ minWidth: 650 }} aria-label="simple table" key={nanoid()}>
+				<TableHead>
+					<TableRow>
+						<TableCell sx={{ fontWeigth: 'bold' }} align="left">Locations (all)</TableCell>
+					</TableRow>
+				</TableHead>
+
+				{locations.map((l) => (
+					<TableBody key={nanoid()}>
+						<TableRow key={nanoid()}>
+							<TableCell>{l.city}</TableCell>
+							<TableCell>{l.latitude}</TableCell>
+							<TableCell>{l.longitude}</TableCell>
+						</TableRow>
+					</TableBody>
+				))}
+			</Table>
+
+
+
 		</TableContainer>
+
 	)
 }
 
