c1a1fa2c5d127167adecae4e3384484ee54e415d
JouniR68 <jriimala@gmail.com>
2024-07-09 12:32:51 +0300

    Confirmation style improvements

--- src/components/Confirmation.jsx
+++ src/components/Confirmation.jsx
@@ -1,11 +1,15 @@
 
 function ConfirmationModal({ onConfirm, onCancel }) {
   return (
-    <div className="modal">
-      <div className="modal-content">
-        <h2>Ok to read your location (not accurate...)?</h2>
-        <button onClick={onConfirm}>Yes</button>
-        <button onClick={onCancel}>No</button>
+    <div className="confirmation-container">
+
+      <div className="confirmation-content">
+        
+        <h2>Ok to read your location ?</h2> 
+        <h4>(Not accurate & will not be used for anything...)</h4>
+        <button id="ok" onClick={onConfirm}>Yes</button>
+        <button id="nok" onClick={onCancel}>No</button>
+        
       </div>
     </div>
   );
