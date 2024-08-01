e46f83e84db24d31926a2c383f8292e8afe6c0cb
JouniR68 <jriimala@gmail.com>
2024-08-01 15:43:38 +0300

    radius change from 100 to 1000 meters

--- src/components/CheckLocation.jsx
+++ src/components/CheckLocation.jsx
@@ -67,7 +67,7 @@ function CheckLocation() {
   const getPlace = async (lat, lon) => {
       try {
         console.log("getPlaces")
-        const response = await fetch(`http://localhost:5000/api/places?location=${lat},${lon}&radius=100&type=restaurant`);
+        const response = await fetch(`http://localhost:5000/api/places?location=${lat},${lon}&radius=1000&type=restaurant`);
         console.log("response: ", response)
         const data = await response.json();
         console.log("Response from the server ", data.results)
