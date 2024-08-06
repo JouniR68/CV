c3d0924485426cfd68888cbc21337f2f63c4f2f3
JouniR68 <jriimala@gmail.com>
2024-08-06 17:44:49 +0300

    firebase config details updated

--- src/firebase.js
+++ src/firebase.js
@@ -8,11 +8,12 @@ import { getAuth } from "firebase/auth"
 const firebaseConfig = {
   apiKey: "AIzaSyDeBnDCtHTEiO9RXwWauj_w9QtjXkfBjz0",
   authDomain: "firma-ed35a.firebaseapp.com",
+  databaseURL: "https://firma-ed35a-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "firma-ed35a",
   storageBucket: "firma-ed35a.appspot.com",
   messagingSenderId: "350355976791",
-  appId: "1:350355976791:web:e8262de3a9a02fca44a912",
-  measurementId: "G-3NRNRNJMH8"
+  appId: "1:350355976791:web:d59133a422dcf4c844a912",
+  measurementId: "G-07WQWF44MZ"
 };
 
 export const app = initializeApp(firebaseConfig);
