341379e2483a5317a5c35954928a3f83448cc270
JouniR68 <jriimala@gmail.com>
2024-09-03 10:26:24 +0300

    tilauskori

--- src/components/services/TheBasket.jsx
+++ src/components/services/TheBasket.jsx
@@ -1,24 +1,38 @@
 import { useLocation } from "react-router-dom";
+import Button from '@mui/material/Button';
 
 const TheBasket = () => {
     const location = useLocation();
 
-    const {state} = location
-    const {data} = state;
-    const {title, description, kpl, priceh} = data
+    const { state } = location
+    const { data } = state;
+    const { title, description, kpl, priceh } = data
 
     console.log("TheBasket ", title)
-        
-    return ( 
-    <>
-    <h1>Tilauskorisi</h1> 
-    <h4>Maksu työn valmistuttua, laskulla</h4>
-        <span>
-            {title} {kpl} tuntia. Kokonaislasku {kpl * priceh}
-        </span>
+
+    let totalPrice = kpl * priceh
+    let withoutTax = totalPrice * 25.5 / 100
+    let taxed = totalPrice - withoutTax;
+
+    return (
+        <>
+            <h1>Tilauskorisi</h1>
+            <h4>Maksu työn valmistuttua, laskulla</h4>
+
+
+            <table className="">
+                <tr><th>Tuote</th><th>Määrä</th><th>Summa</th></tr>
+                <tr><td>{title}</td><td>{kpl}</td><td>{totalPrice}</td></tr>
+                <tr><td></td><td>Veroton hinta</td><td>{withoutTax}</td></tr>
+                <tr><td></td><td>Vero 25.5%</td><td>{taxed}</td></tr>
+                <tr><td></td><td>Yhteensä</td><td>{taxed + withoutTax}</td></tr>
+            </table>
+            <Button variant="contained">Vahvista</Button>
+
+
         </>
     );
 
 }
- 
+
 export default TheBasket;
