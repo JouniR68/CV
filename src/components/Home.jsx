2df1f3bea703bbbec47a6a15fba6a3dc2318bad6
JouniR68 <jriimala@gmail.com>
2024-10-08 16:53:32 +0300

    CSS- improvements

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,47 +8,6 @@ import { Button, Typography } from '@mui/material';
 import InactivityTimer from './InActivity';
 import FeedbackDialog from './Feedback';
 
-
-const TextWrapper = ({ text, maxLength }) => {
-  console.log("text: ", text + ", length: ", maxLength)
-  // Function to split the text into chunks of maxLength without breaking words
-  const splitIntoChunksWithoutBreakingWords = (text, maxLength) => {
-    const words = text.split('.'); // Split the text by spaces (words)
-    const chunks = [];
-    let currentChunk = '';
-
-    words.forEach((word) => {
-      // If adding the next word exceeds maxLength, push the current chunk and start a new one
-      if ((currentChunk + word).length <= maxLength) {
-        currentChunk += (currentChunk ? ' ' : '') + word;
-      } else {
-        chunks.push(currentChunk);
-        currentChunk = word;
-      }
-    });
-
-    // Add the remaining chunk
-    if (currentChunk) {
-      chunks.push(currentChunk);
-    }
-
-    return chunks;
-  };
-
-  // Split the text into chunks without breaking words
-  const chunks = splitIntoChunksWithoutBreakingWords(text, maxLength);
-
-  return (
-    <div>
-      {chunks.map((chunk, index) => (
-        <p key={index}>{chunk}
-          {index !== 0 && '.'}
-        </p> // Render each chunk as a separate <p> tag
-      ))}
-    </div>
-  );
-};
-
 export default function Home() {
   const [isMobile, setMobile] = useState(false);
   const [confirmation, setConfirmation] = useState(true)
@@ -152,8 +111,7 @@ export default function Home() {
   return (
     <div className="home">
 
-    <div className = "home-teksti">{t('Cando1')}<p/>{t('Cando2')}<p/>{t('Cando3')}</div>
-
+      <div className="home-teksti">{t('Cando1')}<p />{t('Cando2')}<p />{t('Cando3')}</div>      
       <div className="home-kollaasi">
         <Typography variant="h5">
           <h5>{t('PM')}</h5>
@@ -171,26 +129,22 @@ export default function Home() {
           <h5>{t('Webdev')}</h5>
           <img alt="Web-koodausta, apuja yms" src="/Images/jrsoft/web.jpg" onClick={() => tarjouspyyntoon()} />
         </Typography>
-        <Typography variant="h5">          
-          <img alt="Palaute/Feedback" src="/Images/jrsoft/feedback.png" onClick={() => handleOpen()} />
+        <Typography variant="h5">
           <h5>{t('Message')}</h5>
+          <img alt="Palaute/Feedback" src="/Images/jrsoft/feedback.png" onClick={() => handleOpen()} />
         </Typography>
         <Typography variant="h5">
-          
-          <img alt="Vastuuvapaus / disclaimer" src="/Images/jrsoft/disclaimer.png" onClick={showDisclaimer}></img>
           <h5>{t('GoodToKnow')}</h5>
+          <img alt="Vastuuvapaus / disclaimer" src="/Images/jrsoft/disclaimer.png" onClick={showDisclaimer}></img>
         </Typography>
-
       </div>
 
       {isLoggedIn && <InactivityTimer />}
-      {/*viewportWidth*/} {/*viewportHeight*/}
+
 
       {disclaimer && navigate('/done', { state: { description: "disclaimer" } })}
       {/* Feedback Dialog */}
       <FeedbackDialog open={dialogOpen} handleClose={handleClose} />
-
-
     </div>
   );
 }
